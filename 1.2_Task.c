ca9c485aaf87e4c1bd32d2da0f04495a28ec0453
OmTaywade-Rizvi-College-id <omstaywade@eng.rizvi.edu.in>
2025-10-12 12:08:21 +0530

    Update and rename 1.1_Task.c to 1.2_Task.c

---
+++ 1.2_Task.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+      // My Name
+    printf( "Name :Om Satish Taywade \n");
+    
+    // My age
+    printf("Age : 18 \n");
+    
+    // My Class
+    printf("Class: FYCE \n");
+
+    // My Division
+    printf("Division: C \n");
+    
+    // My UIN
+    printf("Uin: 251P027 \n");
+    
+    float phyics,chemistry,maths, sum;
+    float average;
+    printf("physics :");
+    scanf("%f",&phyics);
+    printf("chemistry :");
+    scanf("%f",&chemistry);
+    printf("math :");
+    scanf("%f",&maths);
+    sum = phyics + chemistry + maths;
+    printf("sum =%f \n", sum);
+    average =sum/3;
+    printf("average =%f\n",average);
+
+    if (average>=50){
+  printf("You are Eligible");
+      }
+     else{
+  printf("You are Not Eligible");
+     }
+    return 0;
+
+}
